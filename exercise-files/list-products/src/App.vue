50e84e2769dc6c4ec25099a7502c370529c99ee2
christinto <chrisperrymedia@gmail.com>
2018-06-26 00:06:24 +1000

    working

--- exercise-files/list-products/src/App.vue
+++ exercise-files/list-products/src/App.vue
@@ -1,8 +1,30 @@
+<!-- src/App.vue -->
 <template>
   <!-- when using a component in a template Vue.js expects the DOM element to be
   lowercased and dasherized. e.g. ProductList -> product-list -->
 
-  <!-- use the <product-list> component here! -->
+  <product-list></product-list>
+</template>
+
+<script>
+  import ProductList from './components/ProductList'
+
+  export default {
+    components: {
+      // With ES6 we can use the Object Literal Shorthand Syntax to save us some
+      // keystrokes
+      ProductList
+    }
+  }
+</script>
+
+<!--
+ORIGINAL
+<template>
+  !-- when using a component in a template Vue.js expects the DOM element to be
+  lowercased and dasherized. e.g. ProductList -> product-list --
+
+  use the <product-list> component here!
 </template>
 
 <script>
@@ -14,3 +36,5 @@ export default {
   }
 }
 </script>
+
+-->
